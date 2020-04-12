632119fa91fc496c0dddda07fadec64a78ada690
Max Zylstra <maxzylstra@gmail.com>
2020-04-11 20:17:40 -0500

    bones that need changing

--- textfromphoto.py
+++ textfromphoto.py
@@ -6,6 +6,9 @@ class Photo():
 
 	def __init__(self, image):
 		self.image = image
+	
+	def resize(self, image):
+		self.image = cv2.resize(warped, (1350, 1150))
 
 
 #Need to find framework to read text from photo.
