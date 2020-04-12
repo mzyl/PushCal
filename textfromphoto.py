a7f33f0e9e1165e9346dce11e94bc5e6d9045bdc
mzyl <maxzylstra@gmail.com>
2020-04-11 22:16:20 -0500

    Added dependencies

--- textfromphoto.py
+++ textfromphoto.py
@@ -1,11 +1,12 @@
 #I think this will be the most challenging part of the project. 
-
+from PIL import Image
+import pytesseract
 
 #Need to accept photo as input.
 class Photo():
 
 	def __init__(self, image):
-		self.image = image
+		self.data = Image.open(image)
 	
 	def resize(self, image):
 		self.image = cv2.resize(warped, (1350, 1150))
