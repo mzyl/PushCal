0d4a9eac7b4190e8e3dce4dedda79162d37e683e
Max Zylstra <maxzylstra@gmail.com>
2020-04-20 15:08:10 -0500

    Added separate functions for image manipulation: grayscale, blur, etc.

--- readimage.py
+++ readimage.py
@@ -9,10 +9,27 @@ def pathToImage(path):
 #Creates an edged image for easier OCR
 #Not actually great for OCR, but may be useful later
 def toEdged(image):
-	image = imutils.resize(image, height = 500)
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	image = cv2.GaussianBlur(image, (5, 5), 0)
 	return cv2.Canny(image, 75, 200)
+
+#Resize image
+def resize(image, height):
+	return imutils.resize(image, height = height)
+
+#Gaussian Blur
+def guassian(image):
+	return cv2.GaussianBlur(image, (5, 5), 0)
+
+#Noise removal
+def removeNoise(image):
+	return cv2.medianBlur(image, 5)
+
+#Thresholding - Not sure what this does yet
+def thresholding(image):
+	return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#Grayscale
+def gray(image):
+	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #Reads text from image to be stored in a string
 def toText(image):
@@ -29,4 +46,9 @@ def splitDays(image):
 #Makes a separate image for the times to be read from
 def splitTimes(image):
 	return image[0:, int(image.shape[1]/2):]
+
+#config = r'--oem 3 -psm *'
+#pytesseract.image_to_string(x, config=config)
+#psm 3 works best for Days
+#psm 12 works best for Times
 
