0c5fee59e9355b378a82d38e2cbe2c990cf4eafc
mzyl <maxzylstra@gmail.com>
2020-04-20 22:38:36 -0500

    Added functions for later use

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #Main will glue the pieces together when finished. 
 
-import textfromphoto as tfp
-import parsetext as parse
-import pushdate as push
+import readimage as img
+
+#Return separate image containing the space of days
+def splitDays(image):
+	return image[0:, 0:int(image.shape[1]/2)]
+
+#Return separate image containing the space of times
+def splitTimes(image):
+	return image[0:, int(image.shape[1]/2:]
+
+#notes for tesseract
+#config = r'--oem 3 --psm *'
+#pytesseract.image_to_string(x, config=config)
+#psm 3 (default) works best for Days
+#psm 12 works best for Times
