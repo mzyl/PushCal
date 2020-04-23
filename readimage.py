e32813137fc04addfa64de0ce00029de9e475333
Max Zylstra <maxzylstra@gmail.com>
2020-04-23 13:25:12 -0500

    moved all functions back out of class

--- readimage.py
+++ readimage.py
@@ -2,52 +2,44 @@ import cv2
 import imutils
 import pytesseract
 
-#Maybe a subclass should be made to handle image modification?
+#Read image file
+def input(path):
+	return cv2.imread(path)
 
-class Image():
+#Pull text from image
+def read(image):
+	return pytesseract.image_to_string(image)
 
-	#Read image file
-	def __init__(self, path):
-		self.image = cv2.imread(path)
+#Write image to file
+def write(image, filename = "modified.jpg"):
+	cv2.imwrite(filename, image)
 
-	#Return image contents
-	def ret(self):
-		return self.image
+#Crop image to spec
+def crop(image, left = 0, right = 0, top = 0, bottom = 0):
+	return image[0:, 0:int(self.image.shape[1]/2)]
 
-	#Pull text from image
-	def read(self):
-		return pytesseract.image_to_string(self.image)
+#Resize image
+def resize(image, height):
+	return imutils.resize(image, height = height)
 
-	#Write image to file
-	def write(self, filename = "modified.jpg"):
-		cv2.imwrite(filename, self.image)
+#Grayscale
+def grayscale(image):
+	return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	#Crop image to spec
-	def crop(self, left = 0, right = 0, top = 0, bottom = 0):
-		self.image = self.image[0:, 0:int(self.image.shape[1]/2)]
+#Gaussian Blur
+def guassian(image):
+	return cv2.GaussianBlur(image, (5, 5), 0)
 
-	#Resize image
-	def resize(self, height):
-		self.image = imutils.resize(self.image, height = height)
-	
-	#Grayscale
-	def gray(self):
-		self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+#Creates an edged image for easier OCR
+def edged(image):
+	return cv2.Canny(image, 75, 200)
 
-	#Gaussian Blur
-	def guassian(self):
-		self.image = cv2.GaussianBlur(self.image, (5, 5), 0)
+#Noise removal
+def removeNoise(image):
+	return cv2.medianBlur(image, 5)
 
-	#Creates an edged image for easier OCR
-	def edged(self):
-		self.image = cv2.Canny(self.image, 75, 200)
-
-	#Noise removal
-	def removeNoise(self):
-		self.image = cv2.medianBlur(self.image, 5)
-
-	#Thresholding - Not sure what this does yet -- Errors anyway
-	def thresholding(self):
-		self.image = cv2.threshold(self.image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+#Thresholding - Not sure what this does yet -- Errors anyway
+def thresholding(image):
+	return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 
