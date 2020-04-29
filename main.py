03350f98345abbcbfbce174f053e433882f77287
mzyl <maxzylstra@gmail.com>
2020-04-29 01:30:12 -0500

    added notes for perspective transform

--- main.py
+++ main.py
@@ -20,3 +20,19 @@ def splitTimes(image):
 #psm 3 (default) works best for Days
 #psm 12 works best for Times
 
+'''
+***Transforming image notes:***
+pts1 = np.float32([[points.item(0)+20,points.item(1)-20],[points.item(2)+20,points.item(3)+20],[points.item(4)-20,points.item(5)+20],[points.item(6)-20,points.item(7)-20]])
+pts2 = np.float32([[2500,0],[2500,4000],[0,4000],[0,0]])
+M = cv2.getPerspectiveTransform(pts1,pts2)
+dst = cv2.warpPerspective(image,M,(2510,4000))
+
+transCopy = dst.copy()
+trans = dst.copy()
+trans = img.grayscale(trans)
+trans = img.edged(trans)
+cnts = img.findContours(trans)
+cnts = img.sortContours(cnts)
+img.drawContours(transCopy, cnts)
+img.write(transCopy)
+'''
