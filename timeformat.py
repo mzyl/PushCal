13e82b97909c9bd8a4624e7038d728065e63aa97
Max Zylstra <maxzylstra@gmail.com>
2020-05-21 23:03:28 -0500

    removed test prints

--- timeformat.py
+++ timeformat.py
@@ -16,32 +16,22 @@ def calc_end_time_begin(time):
 	list = []
 	ret = ''
 	if time[0] == '0':
-		print("if")
 		for i in time:
 			list.append(i)
-		print(list)
 		list[1] = str(int(list[1]) + 1)
-		print(list)
 		return ret.join(list)
 
 	else:
-		print("else")
 		if time[1] == '2':
-			print("if2")
 			for i in time:
 				list.append(i)
-			print(list)
 			list[0] = '0'
 			list[1] = '1'
-			print(list)
 			return ret.join(list)
 		
 		else:
-			print("else2")
 			for i in time:
 				list.append(i)
-			print(list)
 			list[1] = str(int(list[1]) + 1)
-			print(list)
 			return ret.join(list)
 
