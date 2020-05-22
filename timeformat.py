d0b43e829defc0558644b533e2e8fe1c1b1b8006
mzyl <maxzylstra@gmail.com>
2020-05-21 22:40:17 -0500

    working on function for calculating endTimeBegin

--- timeformat.py
+++ timeformat.py
@@ -7,8 +7,32 @@ def format(time):
 				list.append(i)
 			list.insert(0, '0')
 			return ret.join(list)
-
 		else: raise
 
 	except:
 		return time
+
+def calc_end_time_begin(time):
+	list = []
+	ret = ''
+	if time[0] == '0':
+		print("if")
+		for i in time:
+			list.append(i)
+		print(list)
+		list[1] = str(int(list[1]) + 1)
+		print(list)
+		return ret.join(list)
+
+	else:
+		print("else")
+		if time[1] == '2':
+			time[0] = 0
+			time[1] = 1
+			print(time)
+			return time
+		
+		else:
+			time[1] = int(time[1]) + 1
+			print(time)
+			return time
