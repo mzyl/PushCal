8cc609670670ba74f20807cdcb64726b31f446e4
Max Zylstra <maxzylstra@gmail.com>
2020-05-21 23:00:44 -0500

    endTimeBegin calculation working

--- timeformat.py
+++ timeformat.py
@@ -27,12 +27,21 @@ def calc_end_time_begin(time):
 	else:
 		print("else")
 		if time[1] == '2':
-			time[0] = 0
-			time[1] = 1
-			print(time)
-			return time
+			print("if2")
+			for i in time:
+				list.append(i)
+			print(list)
+			list[0] = '0'
+			list[1] = '1'
+			print(list)
+			return ret.join(list)
 		
 		else:
-			time[1] = int(time[1]) + 1
-			print(time)
-			return time
+			print("else2")
+			for i in time:
+				list.append(i)
+			print(list)
+			list[1] = str(int(list[1]) + 1)
+			print(list)
+			return ret.join(list)
+
