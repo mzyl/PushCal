67735ce3ca0a5214565a37aea1727fd05287b190
Max Zylstra <maxzylstra@gmail.com>
2020-05-24 18:56:24 -0500

    formats mo betta

--- timeformat.py
+++ timeformat.py
@@ -1,22 +1,27 @@
 #Adds '0' at beginning of time if needed
 def format(time):
+	list = []
+	ret = ''
 	try:
 		if time[1] == ':':
-			list = []
-			ret = ''
 			for i in time:
 				list.append(i)
 			list.insert(0, '0')
+			list.extend(':' + '0' + '0')
 			return ret.join(list)
 		else: raise
 
 	except:
-		return time
+		for i in time:
+			list.append(i)
+		list.extend(':' + '0' + '0')
+		return ret.join(list)
 
 #Calculates the endTimeBegin attribute based on startTimeFinish as input
 def calc_end_time_begin(time):
 	list = []
 	ret = ''
+	
 	if time[0] == '0':
 		for i in time:
 			list.append(i)
