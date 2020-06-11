5603d1364c237a45e5a785caf1d7f86d5166e393
mzyl <maxzylstra@gmail.com>
2020-06-11 18:40:06 -0500

    retab

--- main.py
+++ main.py
@@ -5,43 +5,43 @@ import dates
 
 #Called by web app to take image as input and return read data as string
 def output(image):
-	return img.read(img.input(image))
+    return img.read(img.input(image))
 
 #Transform image based on calendar outline for ease of splitting information into individual days
 def transform_image(image):
-	copy = image.copy()
-	image = img.prep_for_contours(image)
-	contours = img.do_contours(image)
-	print(contours)
-	return img.transform(copy, contours)
+    copy = image.copy()
+    image = img.prep_for_contours(image)
+    contours = img.do_contours(image)
+    print(contours)
+    return img.transform(copy, contours)
 
 #Generates an array of Date objects to hold the upcoming Date instances
 days = [dates.Date() for i in range(7)]
 
 #Crops calendar into individual days and populates Date instances with the information
 def gen_class(image):
-	crop_size = int(image.shape[0]/7)
-	top = 0
-	bottom = crop_size
-	for i in range(len(days)):
-		cropped = img.crop(image, top = top, bottom = bottom)
-		text = img.read(cropped).split()
-		days[i].assignDay(text)
-		days[i].assignTime(text)
-		days[i].clean()
-		days[i].assignBlock()
-		#print(days[i].toString())
-		top = bottom
-		bottom = bottom + crop_size
+    crop_size = int(image.shape[0]/7)
+    top = 0
+    bottom = crop_size
+    for i in range(len(days)):
+        cropped = img.crop(image, top = top, bottom = bottom)
+        text = img.read(cropped).split()
+        days[i].assignDay(text)
+        days[i].assignTime(text)
+        days[i].clean()
+        days[i].assignBlock()
+        #print(days[i].toString())
+        top = bottom
+        bottom = bottom + crop_size
 
 #def main(image):
 def main():
-	image = img.image('imgzoom.jpg') #Remove after testing
-	image = transform_image(image)
-	img.write(image)
-	gen_class(image)
-	for i in range(len(days)):
-		print(days[i].toString())
+    image = img.image('imgzoom.jpg') #Remove after testing
+    image = transform_image(image)
+    img.write(image)
+    gen_class(image)
+    for i in range(len(days)):
+        print(days[i].toString())
 
 ''' To Do:
 Make img.transform points more dynamic
